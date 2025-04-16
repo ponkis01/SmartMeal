a84f5715d241030b25d261470603097b7887fecd
ponkis01 <152533644+ponkis01@users.noreply.github.com>
2025-04-16 12:51:11 +0200

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,1 +1,12 @@
+import os
+from dotenv import load_dotenv
 
+# .env laden
+load_dotenv()
+
+# Zugriff auf den Key
+API_KEY = os.getenv("SPOONACULAR_API_KEY")
+
+# Test (nur zur Sicherheit)
+if not API_KEY:
+    print("❌ API key not found. Make sure it's set in the .env file.")
